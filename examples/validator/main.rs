8cc0b1fa9f42e74ca05d4450b96d9f3b3ec49f5e
Jasper 张汉杰 <58722459+VainJoker@users.noreply.github.com>
2024-11-11 17:40:11 +0800

    🔨 chore: Introduce data validation example
    A new example showcasing data validation has been added.  This example demonstrates how to use the `validator` crate with `realme` to validate user-provided data according to specified rules.  The validation includes checks for email and URL formats, string length, numeric ranges, and a custom validation function for usernames.

---
+++ examples/validator/main.rs
@@ -0,0 +1,56 @@
+#[cfg(feature = "toml")]
+fn main() {
+    use realme::prelude::*;
+    use serde::Deserialize;
+    use validator::{Validate, ValidationError};
+
+    #[derive(Debug, Validate, Deserialize)]
+    struct SignupData {
+        #[validate(email)]
+        mail: String,
+        #[validate(url)]
+        site: String,
+        #[validate(length(min = 1), custom(function = "validate_unique_username"))]
+        #[serde(rename = "firstName")]
+        first_name: String,
+        #[validate(range(min = 18, max = 20))]
+        age: u32,
+        #[validate(range(exclusive_min = 0.0, max = 100.0))]
+        height: f32,
+    }
+
+    fn validate_unique_username(username: &str) -> Result<(), ValidationError> {
+        if username == "xXxShad0wxXx" {
+            // the value of the username will automatically be added later
+            return Err(ValidationError::new("terrible_username"));
+        }
+    
+        Ok(())
+    }
+    
+    let c = String::from(
+        r#"
+        mail = "xXxShad0wxXx@gmail.com"
+        site = "https://xXxShad0wxXx.com"
+        firstName = "xXxShad0wxXx"
+        age = 18
+        height = 180.0
+        "#,
+    );
+
+    let realme = Realme::builder()
+        .load(Adaptor::new(StringSource::<TomlParser>::new(&c)))
+        .build()
+        .expect("Building configuration object");
+
+    let data: SignupData = realme.try_deserialize().expect("deserialize failed");
+
+    println!("{:#?}",data.validate());
+    
+}
+
+#[cfg(not(feature = "toml"))]
+fn main() {
+    println!("Please enable toml feature");
+    println!("cargo run --example simple --features toml");
+}
